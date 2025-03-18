d397af4a16ae239a6623154734ae630fae90bcd9
PauloSenise <paulo.senise@hotmail.com>
2025-03-18 10:24:45 -0300

    Update previsao_tempo.R
    Implementação do código para coletar mais dados climáticos.

--- Previsão_Tempo/Previsão do Tempo/previsao_tempo.R
+++ Previsão_Tempo/Previsão do Tempo/previsao_tempo.R
@@ -6,6 +6,7 @@ library(jsonlite)
 # Definir a chave da API e a cidade 
 api_key <- "9b90edf9b722e841505a711976022ea2" 
 cidade <- URLencode("São Paulo")
+
 url <- paste0("http://api.openweathermap.org/data/2.5/weather?q=", {cidade}, 
               "&appid=", {api_key}, "&units=metric&lang=pt")
 
@@ -17,15 +18,22 @@ if (status_code(resposta) == 200) {
   dados <- content(resposta, as = "text", encoding = "UTF-8")
   dados_json <- fromJSON(dados, simplifyVector = FALSE)  # Garante que retorna listas
   
-  # Verificar estrutura da resposta
+# Verificar estrutura da resposta
   #str(dados_json)
   
-  # Extrair os dados corretamente
+# Extrair os dados corretamente
   temperatura <- dados_json$main$temp
   descricao <- dados_json$weather[[1]]$description  # Acessando corretamente o primeiro item da lista
+  velocidade <- dados_json$wind$speed
+  temp_max <- dados_json$main$temp_max
+  temp_min <- dados_json$main$temp_min
   
+# Imprimir na Tela os Dados Climáticos
   cat("Temperatura em", "(",dados_json$name,"):", temperatura, "°C\n")
-  cat("Condição:", descricao, "\n")
+  cat("Temperatura máx:", temp_max, "C°\n")
+  cat("Temperatura mín:", temp_min, "C°\n")
+  cat("Condição do Clima:", descricao, "\n")
+  cat("Velicidade do Vento:", velocidade, "m/s\n")
 } else {
   cat("Erro ao buscar os dados. Código de status:", status_code(resposta), "\n")
 }
